d2f1a0b2ff67d3efe2f16392a0bbfcf25702a75c
Yicun Hou <yicunhou@bu.edu>
2020-03-06 09:32:04 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_05_210139) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
